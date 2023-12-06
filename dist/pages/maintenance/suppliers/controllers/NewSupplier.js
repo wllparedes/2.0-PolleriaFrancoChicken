5d94ae353a6e5a9e165a5b2371308d0241dbd0ee
Walin Paredes <walinparedes3010@gmail.com>
2023-12-05 23:20:01 -0500

    mensaje formilario incorrecto

--- dist/pages/maintenance/suppliers/controllers/NewSupplier.js
+++ dist/pages/maintenance/suppliers/controllers/NewSupplier.js
@@ -1,90 +1,99 @@
 /** @format */
 
 // ? MANTENIMIENTO DE CLIENTES
-import { expresiones } from "../../../../assets/js/global/exprecionesRegulares.js";
+import { expresiones } from '../../../../assets/js/global/exprecionesRegulares.js';
 import {
-  validarCampo,
-  campos,
-} from "../../../../assets/js/global/validarCampos.js";
+	validarCampo,
+	campos,
+} from '../../../../assets/js/global/validarCampos.js';
 
 $(document).ready(() => {
-  //  Seleccionar Elementos DOM ( contenedor__mensaje / all inputs )
+	//  Seleccionar Elementos DOM ( contenedor__mensaje / all inputs )
 
-  let contenedor_mensaje = document.getElementById("contenedor__mensaje");
-  const inputs = document.querySelectorAll("#formulario input");
-  // Start Validación del formulario
+	let contenedor_mensaje = document.getElementById('contenedor__mensaje');
+	const inputs = document.querySelectorAll('#formulario input');
+	// Start Validación del formulario
 
-  const validarFormulario = (e) => {
-    switch (e.target.name) {
-      case "razon_social":
-        validarCampo(expresiones.razon_social, "razon_social", e.target);
-        break;
-      case "direccion":
-        validarCampo(expresiones.direccion, "direccion", e.target);
-        break;
-      case "ruc":
-        validarCampo(expresiones.ruc, "ruc", e.target);
-        break;
-      case "phone":
-        validarCampo(expresiones.phone, "phone", e.target);
-        break;
-      case "email":
-        validarCampo(expresiones.email, "email", e.target);
-        break;
-    }
-  };
+	const validarFormulario = (e) => {
+		switch (e.target.name) {
+			case 'razon_social':
+				validarCampo(
+					expresiones.razon_social,
+					'razon_social',
+					e.target
+				);
+				break;
+			case 'direccion':
+				validarCampo(expresiones.direccion, 'direccion', e.target);
+				break;
+			case 'ruc':
+				validarCampo(expresiones.ruc, 'ruc', e.target);
+				break;
+			case 'phone':
+				validarCampo(expresiones.phone, 'phone', e.target);
+				break;
+			case 'email':
+				validarCampo(expresiones.email, 'email', e.target);
+				break;
+		}
+	};
 
-  inputs.forEach((input) => {
-    input.addEventListener("keyup", validarFormulario);
-    input.addEventListener("blur", validarFormulario);
-  });
+	inputs.forEach((input) => {
+		input.addEventListener('keyup', validarFormulario);
+		input.addEventListener('blur', validarFormulario);
+	});
 
-  // End
+	// End
 
-  // Cuando se de Submit en el Btn Registrar
+	// Cuando se de Submit en el Btn Registrar
 
-  $("#formulario").submit(function (e) {
-    e.preventDefault();
+	$('#formulario').submit(function (e) {
+		e.preventDefault();
 
-    if (
-      campos.razon_social &&
-      campos.direccion &&
-      campos.ruc &&
-      campos.phone &&
-      campos.email
-    ) {
-      // Ajax
-      const postData = {
-        razon_social: $("#razon_social").val(),
-        direccion: $("#direccion").val(),
-        ruc: $("#ruc").val(),
-        phone: $("#phone").val(),
-        email: $("#email").val(),
-      };
+		if (
+			campos.razon_social &&
+			campos.direccion &&
+			campos.ruc &&
+			campos.phone &&
+			campos.email
+		) {
+			// Ajax
+			const postData = {
+				razon_social: $('#razon_social').val(),
+				direccion: $('#direccion').val(),
+				ruc: $('#ruc').val(),
+				phone: $('#phone').val(),
+				email: $('#email').val(),
+			};
 
-      $.ajax({
-        url: "../models/newSupplier.php",
-        type: "POST",
-        data: postData,
-        success: function (response) {
-
-          let respuesta = response.trim();
-          console.log(respuesta);
-          if (respuesta === "error") {
-            no_registrado("proveedor");
-          } else {
-            //
-            document.querySelectorAll("#formulario input").forEach((i) => {
-              i.classList.remove("is-valid", "is-invalid");
-            });
-            si_registrado();
-            $("#formulario").trigger("reset");
-            // redireccionar('lista-usuarios');
-          }
-        },
-      });
-    } else {
-      // contenedor_mensaje.classList.add('contenedor__mensaje-activo');
-    }
-  });
+			$.ajax({
+				url: '../models/newSupplier.php',
+				type: 'POST',
+				data: postData,
+				success: function (response) {
+					let respuesta = response.trim();
+					console.log(respuesta);
+					if (respuesta === 'error') {
+						no_registrado('proveedor');
+					} else {
+						//
+						document
+							.querySelectorAll('#formulario input')
+							.forEach((i) => {
+								i.classList.remove('is-valid', 'is-invalid');
+							});
+						si_registrado();
+						$('#formulario').trigger('reset');
+						redireccionar('listSuppliers');
+						contenedor_mensaje.classList.remove(
+							'contenedor__mensaje-activo'
+						);
+						contenedor_mensaje.classList.add('contenedor__mensaje');
+					}
+				},
+			});
+		} else {
+			contenedor_mensaje.classList.add('contenedor__mensaje-activo');
+		}
+	});
 });
