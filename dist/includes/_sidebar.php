6ec98949e2ccfa70ffb81b862b8eba5d54ad6b30
jeremy <cesarjeremytellomendiola@gmail.com>
2023-12-14 11:21:39 -0500

    link corregido del logo mazer

--- dist/includes/_sidebar.php
+++ dist/includes/_sidebar.php
@@ -3,7 +3,7 @@
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
                 <div class="logo">
-                    <a href="index.html"><img src="./../../../../assets/images/logo/logo.png" alt="Logo"
+                    <a href="./../../../home/storekeeper/views/index.php"><img src="./../../../../assets/images/logo/logo.png" alt="Logo"
                             srcset="" /></a>
                 </div>
                 <div class="toggler">
