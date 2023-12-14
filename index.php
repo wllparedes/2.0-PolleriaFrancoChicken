58085762e6555e3dd99d22e6b2c1e52ae78064b8
jeremy <cesarjeremytellomendiola@gmail.com>
2023-12-13 20:23:23 -0500

    animacion en login

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <link rel="stylesheet" href="dist/assets/css/login/login.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" href="dist/assets/img/login/icon.png" type="image/png">
     <title>Polleria | Franco Chicken </title>
 </head>
