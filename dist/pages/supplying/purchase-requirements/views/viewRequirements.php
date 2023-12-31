0de38195721ad0a99c018d68d84c1f0db93b9734
Walin Paredes <walinparedes3010@gmail.com>
2023-12-31 15:16:45 -0500

    Refactor purchase requirements view and controller

--- dist/pages/supplying/purchase-requirements/views/viewRequirements.php
+++ dist/pages/supplying/purchase-requirements/views/viewRequirements.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog modal-dialog-centered  modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel33">Formulario para ver el Requerimiento </h4>
+                <h4 class="modal-title" id="myModalLabel33">Detalle del requirimiento de compra </h4>
                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <i data-feather="x"></i>
                 </button>
@@ -12,22 +12,9 @@
             <form id="viewRequirements">
                 <div class="modal-body">
                     <div class="invoice">
-                        <div class="invoice-print">
+                        <div class="invoice-print p-3">
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <div class="section-title d-flex justify-content-between">
-                                        <strong>
-                                            Requerimiento de Compra
-                                        </strong>
-                                        <div class="badge badge-light">
-                                            <strong>
-                                            Requerimiento de Compra #
-                                            </strong>
-                                            <span id="id_requirement"></span>
-                                            
-                                        </div>
-                                    </div>
-                                    <hr>
                                     <div class="row">
                                         <div class="col-md-6">
                                             <address>
@@ -61,13 +48,11 @@
                                 </div>
                             </div>
 
-                            <div class="row mt-4">
+                            <div class="row">
                                 <div class="col-md-12">
-                                    <div class="section-title">Detalle del Requerimiento de Compra</div>
                                     <p class="section-lead">Todos los productos del Requerimiento de compra.</p>
                                     <div class="table-responsive">
 
-                                        <!-- ? Tabla a elegir  -->
                                         <table id="table-product" class="table table-striped table-hover table-md">
                                             <thead>
                                                 <tr>
@@ -78,25 +63,21 @@
                                                     <th>Cantidad</th>
                                                 </tr>
                                             </thead>
-                                            <tbody id="body-product">
-                                            
-                                            </tbody>
-
                                         </table>
-
-                                    <!-- ? Fin de la tabla a elegir -->
 
                                     </div>
                                     <div class="row mt-4">
                                         <div class="col-lg-8">
                                             <div class="section-title">Monto del Requerimiento de Compra: </div>
-                                            <p class="section-lead"> <strong>Nota: </strong>El monto del requerimiento de compra no incluye el IGV.</p>
+                                            <p class="section-lead"> <strong>Nota: </strong>El monto del requerimiento
+                                                de compra no incluye el IGV.</p>
                                         </div>
                                         <div class="col-lg-4 text-right">
                                             <hr class="mt-2 mb-2">
                                             <div class="invoice-detail-item">
                                                 <div class="invoice-detail-name">Sub Total</div>
-                                                <div class="invoice-detail-value invoice-detail-value-lg">S/. <span id="subtotal"></span> </div>
+                                                <div class="invoice-detail-value invoice-detail-value-lg">S/. <span
+                                                        id="subtotal"></span> </div>
                                             </div>
                                         </div>
                                     </div>
