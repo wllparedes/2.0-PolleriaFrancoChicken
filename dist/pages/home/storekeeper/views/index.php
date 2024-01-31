62e15899752a054b100bed91a8f2793c5c283213
Walin Paredes <walinparedes3010@gmail.com>
2024-01-31 10:28:59 -0500

    Añadí gráfico para visualizar la cantidad de ordenes de compras por mes

--- dist/pages/home/storekeeper/views/index.php
+++ dist/pages/home/storekeeper/views/index.php
@@ -163,147 +163,10 @@ $informationDashboard = new InformationDashboard();
 							<div class="col-12">
 								<div class="card" id="profile">
 									<div class="card-header" id="cardHeader">
-										<h4>Profile Visit</h4>
+										<h4>Ordenes de compras por mes</h4>
 									</div>
 									<div class="card-body">
-										<div id="chart-profile-visit"></div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-12 col-xl-4">
-								<div class="card" id="profile">
-									<div class="card-header" id="cardHeader">
-										<h4>Profile Visit</h4>
-									</div>
-									<div class="card-body" id="cardBody">
-										<div class="row">
-											<div class="col-6">
-												<div class="d-flex align-items-center">
-													<svg class="bi text-primary" width="32" height="32" fill="blue"
-														style="width: 10px">
-														<use
-															xlink:href="./../../../../assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
-													</svg>
-													<h5 class="mb-0 ms-3">
-														Europe
-													</h5>
-												</div>
-											</div>
-											<div class="col-6">
-												<h5 class="mb-0">862</h5>
-											</div>
-											<div class="col-12">
-												<div id="chart-europe"></div>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-6">
-												<div class="d-flex align-items-center">
-													<svg class="bi text-success" width="32" height="32" fill="blue"
-														style="width: 10px">
-														<use
-															xlink:href="./../../../../assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
-													</svg>
-													<h5 class="mb-0 ms-3">
-														America
-													</h5>
-												</div>
-											</div>
-											<div class="col-6">
-												<h5 class="mb-0">375</h5>
-											</div>
-											<div class="col-12">
-												<div id="chart-america"></div>
-											</div>
-										</div>
-										<div class="row">
-											<div class="col-6">
-												<div class="d-flex align-items-center">
-													<svg class="bi text-danger" width="32" height="32" fill="blue"
-														style="width: 10px">
-														<use
-															xlink:href="./../../../../assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
-													</svg>
-													<h5 class="mb-0 ms-3">
-														Indonesia
-													</h5>
-												</div>
-											</div>
-											<div class="col-6">
-												<h5 class="mb-0">1025</h5>
-											</div>
-											<div class="col-12">
-												<div id="chart-indonesia"></div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-12 col-xl-8">
-								<div class="card" id="profile">
-									<div class="card-header" id="cardHeader">
-										<h4>Latest Comments</h4>
-									</div>
-									<div class="card-body">
-										<div class="table-responsive">
-											<table class="table table-hover table-lg" id="table">
-												<thead>
-													<tr>
-														<th>Name</th>
-														<th>Comment</th>
-													</tr>
-												</thead>
-												<tbody>
-													<tr>
-														<td class="col-3">
-															<div class="d-flex align-items-center">
-																<div class="avatar avatar-md">
-																	<img
-																		src="./../../../../assets/images/faces/5.jpg" />
-																</div>
-																<p class="font-bold ms-3 mb-0">
-																	Si
-																	Cantik
-																</p>
-															</div>
-														</td>
-														<td class="col-auto">
-															<p class="mb-0">
-																Congratulations
-																on your
-																graduation!
-															</p>
-														</td>
-													</tr>
-													<tr>
-														<td class="col-3">
-															<div class="d-flex align-items-center">
-																<div class="avatar avatar-md">
-																	<img
-																		src="./../../../../assets/images/faces/2.jpg" />
-																</div>
-																<p class="font-bold ms-3 mb-0">
-																	Si
-																	Ganteng
-																</p>
-															</div>
-														</td>
-														<td class="col-auto">
-															<p class="mb-0">
-																Wow amazing
-																design! Can
-																you make
-																another
-																tutorial for
-																this design?
-															</p>
-														</td>
-													</tr>
-												</tbody>
-											</table>
-										</div>
+										<div id="chart-orders-for-month"></div>
 									</div>
 								</div>
 							</div>
@@ -329,57 +192,10 @@ $informationDashboard = new InformationDashboard();
 						</div>
 						<div class="card" id="profile">
 							<div class="card-header" id="cardHeader">
-								<h4>Recent Messages</h4>
-							</div>
-							<div class="card-content pb-4">
-								<div class="recent-message d-flex px-4 py-3">
-									<div class="avatar avatar-lg">
-										<img src="./../../../../assets/images/faces/4.jpg" />
-									</div>
-									<div class="name ms-4">
-										<h5 class="mb-1">Hank Schrader</h5>
-										<h6 class="text-muted mb-0">
-											@johnducky
-										</h6>
-									</div>
-								</div>
-								<div class="recent-message d-flex px-4 py-3">
-									<div class="avatar avatar-lg">
-										<img src="./../../../../assets/images/faces/5.jpg" />
-									</div>
-									<div class="name ms-4">
-										<h5 class="mb-1">
-											Dean Winchester
-										</h5>
-										<h6 class="text-muted mb-0">
-											@imdean
-										</h6>
-									</div>
-								</div>
-								<div class="recent-message d-flex px-4 py-3">
-									<div class="avatar avatar-lg">
-										<img src="./../../../../assets/images/faces/1.jpg" />
-									</div>
-									<div class="name ms-4">
-										<h5 class="mb-1">John Dodol</h5>
-										<h6 class="text-muted mb-0">
-											@dodoljohn
-										</h6>
-									</div>
-								</div>
-								<div class="px-4">
-									<button class="btn btn-block btn-xl btn-light-primary font-bold mt-3">
-										Start Conversation
-									</button>
-								</div>
-							</div>
-						</div>
-						<div class="card" id="profile">
-							<div class="card-header" id="cardHeader">
-								<h4>Visitors Profile</h4>
+								<h4>Productos por categoria</h4>
 							</div>
 							<div class="card-body">
-								<div id="chart-visitors-profile"></div>
+								<div id="chart-products-for-category"></div>
 							</div>
 						</div>
 					</div>
@@ -393,6 +209,8 @@ $informationDashboard = new InformationDashboard();
 		</div>
 	</div>
 	<?php include('./../../../../includes/_scripts.php') ?>
+	<script type="module" src="./../controllers/categoryProducts.js"></script>
+	<script type="module" src="./../controllers/qtyPurchaseOrders.js"></script>
 </body>
 
 </html>
