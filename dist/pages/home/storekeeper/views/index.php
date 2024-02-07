2b44fc00ad3e6056f6cd20c61335cfaa6dc2e58f
jeremy <cesarjeremytellomendiola@gmail.com>
2024-02-07 16:43:46 -0500

    borre in id para css que era innecesario

--- dist/pages/home/storekeeper/views/index.php
+++ dist/pages/home/storekeeper/views/index.php
@@ -110,7 +110,7 @@ $informationDashboard = new InformationDashboard();
 													<svg class="icon-dashboard icon icon-tabler icon-tabler-user-shield"
 														width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
 														stroke="currentColor" fill="none" stroke-linecap="round"
-														stroke-linejoin="round" id="icons">
+														stroke-linejoin="round">
 														<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 														<path d="M6 21v-2a4 4 0 0 1 4 -4h2" />
 														<path
